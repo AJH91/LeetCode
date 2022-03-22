67935836d6c83d545b42d105cfbc2f995c9f8c5f
AJH91 <78536426+AJH91@users.noreply.github.com>
2022-03-22 13:07:31 +0000

    Delete maxDepthBinaryTree.py

--- maxDepthBinaryTree.py
+++
@@ -1,9 +0,0 @@
-def maxDepth(self, root):
-    leftCount = 0
-    rightCount = 0
-    leftCount += self.maxDepth(root.left)
-    rightCount += self.maxDepth(root.right)
-    return (leftCount+rightCount)+1
-
-
-
